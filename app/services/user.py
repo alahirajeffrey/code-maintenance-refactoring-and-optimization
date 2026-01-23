7e7d2c3a07d3ce7f486c57e87a4eee2d043147f0
alahirajeffrey <alahirajeffrey@gmail.com>
2026-01-22 22:07:46 -1200

    code refactor

--- app/services/user.py
+++ app/services/user.py
@@ -7,6 +7,6 @@ class UserService:
 
     def get_user_by_id(self, user_id):
         user = self.repository.get_by_id(user_id)
-        if user is None:
-            raise ValueError(f"User with id {user_id} not found")
-        return user
+        if user:
+            return user
+        return None
